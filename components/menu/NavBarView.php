f10a8f7911b9cb459cbc8ada813f5ce8ea8c7d78
Fabrice95500 <fabricecannan@outlook.com>
2025-01-24 00:28:23 +0100

    Fix: Add Group to enseignant navbar.

--- components/menu/NavBarView.php
+++ components/menu/NavBarView.php
@@ -49,7 +49,7 @@ HTML;
 		</div>
 HTML;
 		if (isset($_SESSION['loginUtilisateur'])) {
-		$this->affichage .= <<<HTML
+			$this->affichage .= <<<HTML
 		<div class="sous-menu py-2 d-flex text-white">
 HTML;
 		}
@@ -57,7 +57,7 @@ HTML;
 		$action = $_GET['action'] ?? '';
 
 		$estProfUtilisateur = isset($_SESSION['loginUtilisateur']) && $_SESSION['estProfUtilisateur'] == 1;
-		
+
 		if (isset($_SESSION['loginUtilisateur'])) {
 			$this->affichage .= <<<HTML
 					<a href="index.php?module=home" class="my-auto mx-3 text-decoration-none text-reset">Accueil</a>
@@ -65,26 +65,26 @@ HTML;
 					<a href="index.php?module=rendus&action=home" class="my-auto mx-3 text-decoration-none text-reset">Rendus</a>
 
 HTML;
-            if (isset($_SESSION['estAdmin']))
-                $this->affichage .= <<<HTML
+			if (isset($_SESSION['estAdmin']))
+				$this->affichage .= <<<HTML
                 <a href="index.php?module=paneladmin&action=home" class="my-auto mx-3 text-decoration-none text-reset">Panel Admin</a>
 HTML;
 			if ($module == 'sae' && $action != 'home') {
 				$saeID = $_GET['id'] ?? "";
 				$this->affichage .= <<<HTML
 				|<a href="index.php?module=sae&action=details&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Détails SAE</a>
+				<a href="index.php?module=sae&action=groupe&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Groupe</a>
 HTML;
-			if($estProfUtilisateur)
-			$this->affichage .= <<<HTML
+				if ($estProfUtilisateur)
+					$this->affichage .= <<<HTML
 				<a href="index.php?module=sae&action=listeRendusGroupe&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Fichiers des rendus</a>
 				<a href="index.php?module=sae&action=reponsesAuxChamps&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Réponses aux champs</a>
 				<a href="index.php?module=sae&action=listeSupportGroupe&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Supports des soutenances</a>
 				<a href="index.php?module=sae&action=soutenance&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Soutenance</a>
 
 HTML;
-			if(!$estProfUtilisateur)
-			$this->affichage .= <<<HTML
-				<a href="index.php?module=sae&action=groupe&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Groupe</a>
+				if (!$estProfUtilisateur)
+					$this->affichage .= <<<HTML
 				<a href="index.php?module=sae&action=cloud&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Cloud</a>
 				<a href="index.php?module=sae&action=note&id=$saeID" class="my-auto mx-3 text-decoration-none text-reset">Notes</a>
 HTML;
